b697a5abff0677f9b2f1c4c0507cd34e1c1fddbc
nidhaldring <36518218+nidhaldring@users.noreply.github.com>
2018-09-21 12:19:12 +0100

    fixed some issues

--- worm.cpp
+++ worm.cpp
@@ -1,16 +1,21 @@
 #include<SFML/Graphics.hpp>
 #include<SFML/Window.hpp>
-#include<queue>
+#include<array>
 #include<iostream>
 #include<cstdlib>
 using namespace std;
 using namespace sf;
 #define speed 10
+#define SPECIAL_FOOD_TIME 10
 
 
-
+float mod(float a,int b){
+	if(a>=b)
+		return 0;
+	return a;
+}
 class Food: public Drawable {
-	bool special=rand()%10 ==9;
+	bool special;
 	Vector2f pos;
 	RectangleShape food;
 public:
@@ -23,7 +28,7 @@ public:
 		pos.x=(rand()%80)*10;
 		pos.y=(rand()%60)*10;
 		food.setPosition(pos);
-		special=(rand()%10 == 9);
+		special=(rand()%2 == 0);
 		if(isSpecial())
 			food.setFillColor(Color::Blue);
 		else
@@ -43,126 +48,90 @@ public:
 };
 
 class Worm :public Drawable {
-	int l=8;
-	// len of worm
-	int score=0;
-	mutable queue<RectangleShape*> worm;
-	RectangleShape* head;
-	Vector2f headPos,tailPos;
-	Keyboard::Key dir;
-	Clock clock;
+	int l=50;
+	mutable vector<RectangleShape*> worm;
+	short score=0;
+	Vector2f headPos;
+	Keyboard::Key dir=Keyboard::Key::Up;
 	float dt;
+	Clock clock;
+	bool added=false;
 public:
 	Worm(){
-		head=new RectangleShape(Vector2f(10,10));
-		head->setPosition(300,300);
-		headPos=head->getPosition();
-		worm.push(head);
-		RectangleShape* tmp;
-		for(int i=1;i<l;++i){
-			tmp=new RectangleShape(Vector2f(10,10));
-			tmp->setPosition(300,300+i*10);
-			worm.push(tmp);
+		for(int i=0;i<l;++i){
+			worm.push_back(new RectangleShape(Vector2f(10,10)));
+			worm[i]->setPosition(Vector2f(300,300+i*10));
 		}
-		tailPos=tmp->getPosition();
-		dir=Keyboard::Key::Up;
+		headPos=worm[0]->getPosition();
 		clock.restart();
 	}
 
-	void apply() {
-		//apply to rest
-		// the head should be tail now
-		Vector2f pos,tmp;
-		pos=headPos;
-		for(int i=0;i<l-1;++i){
-			tmp=worm.front()->getPosition();
-			worm.front()->setPosition(pos);
-			worm.push(worm.front());
-			worm.pop();
-			pos=tmp;
-		}
-		tailPos=tmp;
-		// apply to all :v 
+	virtual void draw(RenderTarget& target,RenderStates states=RenderStates::Default) const{
+		for(auto i:worm)
+			target.draw(*i);
 	}
-	//
 	bool moveHead(Keyboard::Key& key){
 		switch(key){
 			case Keyboard::Key::Up:
 				if(dir == Keyboard::Key::Down)
 					return false;
-				head->move(0,-speed);
-				if(head->getPosition().y<=0)
-					head->setPosition(Vector2f(headPos.x,600));
+				if(worm[0]->getPosition().y <= 0)
+					worm[0]->setPosition(worm[0]->getPosition().x,600);
+				worm[0]->move(0,-10);
 				break;
-
 			case Keyboard::Key::Down:
 				if(dir == Keyboard::Key::Up)
 					return false;
-				head->move(0,speed);
-				if(head->getPosition().y>=600)
-					head->setPosition(Vector2f(headPos.x,0));
+				if(worm[0]->getPosition().y >= 600)
+					worm[0]->setPosition(worm[0]->getPosition().x,0);
+				worm[0]->move(0,10);
+				break;
+			case Keyboard::Key::Left:
+				if(dir == Keyboard::Key::Right)
+					return false;
+				if(worm[0]->getPosition().x <= 0)
+					worm[0]->setPosition(800,worm[0]->getPosition().y);
+				worm[0]->move(-10,0);
 				break;
 			case Keyboard::Key::Right:
 				if(dir == Keyboard::Key::Left)
 					return false;
-				head->move(speed,0);
-				if(head->getPosition().x >= 800)
-					head->setPosition(Vector2f(0,headPos.y));
+				if(worm[0]->getPosition().x >= 800)
+					worm[0]->setPosition(0,worm[0]->getPosition().y);
+				worm[0]->move(10,0);
 				break;
-			case Keyboard::Key::Left:
-			if(dir == Keyboard::Key::Right)
-					return false;
-				head->move(-speed,0);
-			if(head->getPosition().x <= 0)
-				head->setPosition(Vector2f(800,headPos.y));
-				break;	
 			default:
-				return false;	
+			return false;				
 		}
 		return true;
 	}
 
+	void apply(){
+		if(added){
+			l++;
+			added=false;
+		}
+		for(int i=l-1;i!=0;--i){
+			worm[i]->setPosition(worm[i-1]->getPosition());
+		}
+	}
 	bool update(Keyboard::Key& key){
-		//dir=key;
 		if(moveHead(key)){
-			dir=key;
-			worm.push(worm.front());
-			worm.pop();
 			apply();
-			headPos=head->getPosition();
+			headPos=worm[0]->getPosition();
+			dir=key;
 			return true;
 		}
 		return false;
-		
 	}
-	// keep on moving 
-	void move(){
-		dt=clock.getElapsedTime().asSeconds();
-		if(dt>=0.05){
-			clock.restart();
-		}
-		else return;
-		moveHead(dir);
-		worm.push(worm.front());
-		worm.pop();
-		apply();
-		cout<<"here"<<endl;
-		headPos=head->getPosition();
-	}
-
 	
-	virtual void draw(RenderTarget& target,RenderStates states=RenderStates::Default) const{
-		RectangleShape* tmp;
-		for(int i=0;i<l;++i){
-			tmp=worm.front();
-			target.draw(*tmp);
-			worm.push(tmp);
-			worm.pop();
+	void keepMoving(){
+		dt=clock.getElapsedTime().asSeconds();
+		if(dt >= 0.048){
+			update(dir);
+			clock.restart();
+			dt=0;
 		}
-	}
-
-	const Vector2f& getPosition() const {
-		return head->getPosition();
 	}
 
 	void eat(Food& food){
@@ -170,23 +139,29 @@ public:
 			score+=10;
 		else
 			score+=3;
-		l+=1;
-		RectangleShape* n= new RectangleShape(Vector2f(10,10));
-		n->setPosition(tailPos);
-		worm.push(n);
+		worm.push_back(new RectangleShape(Vector2f(10,10)));
+		added=true;
 		food.update();
-
 	}
 
-	int getScore() const {
+	short getScore() const {
 		return score;
 	}
 
+	bool operator==(Food& f){
+		return worm[0]->getPosition()==f.getPosition();
+	}
+	bool isDead(){
+		for(int i=0;i<l;++i)
+			for(int j=0;j<l;++j)
+				if(worm[i]->getPosition() == worm[j]->getPosition() && i != j && i>1){
+					return true;
+				}
+			
+		
+		return false;		
+		}
 };
-
-
-
-
 
 int main() {
 
@@ -194,23 +169,45 @@ int main() {
 	Worm worm;
 	Food food;
 	bool updated;
+	Clock clock;
+	float dt=0;
+	bool first=true;
 	while(w.isOpen()){
 		updated=false;
 		Event event;
 		while(w.pollEvent(event)){
 			if(event.type == Event::KeyPressed)
 				updated=worm.update(event.key.code);
+			else if(event.type == Event::Closed)
+				w.close();
 			}
 		if(!updated)
-			worm.move();
-		if(worm.getPosition()==food.getPosition())
+			worm.keepMoving();
+		if(worm == food)
 			worm.eat(food);
 		w.clear();
 		w.draw(worm);
+		if(food.isSpecial()){
+			if(first){
+				first=false;
+				clock.restart();
+			}
+			dt=clock.getElapsedTime().asSeconds();
+			if(dt >= SPECIAL_FOOD_TIME ){
+				food.update();
+				first=true;
+			}
+		}
+		
 		w.draw(food);
 		w.display();
 		cout<<worm.getScore()<<endl;
-		if(worm.getScore() >= 40){
+		if(worm.isDead()){
+			cout<<" u've lost "<<endl;
+			w.close();
+		}
+
+		if(worm.getScore() >= 50){
 			cout<<" u've win"<<endl;
 			w.close();
 		}
